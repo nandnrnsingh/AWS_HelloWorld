c14c58f59facd943798151051967e7e01d56ad89
Nand Narayan Singh <nandnarayan_singh@epam.com>
2025-03-06 19:57:09 +0530

    task 2 completed

---
+++ task02/app/lambdas/hello_world/index.js
@@ -0,0 +1,24 @@
+exports.handler = async (event) => {
+    const path = event.rawPath || event.path;
+    const method = event.httpMethod || event.requestContext?.http?.method;
+ 
+    if (path === "/hello" && method === "GET") {
+        return {
+            statusCode: 200,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                statusCode: 200,
+                message: "Hello from Lambda"
+            }),
+        };
+    }
+ 
+    return {
+        statusCode: 400,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            statusCode: 400,
+            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+        }),
+    };
+};
